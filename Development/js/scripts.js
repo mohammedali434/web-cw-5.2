aec38b524ce7bcaaf28cd108e7a39a8b7f7d690f
mohammedali434 <66567880+mohammedali434@users.noreply.github.com>
2020-06-29 12:09:08 -0700

    انتهى الواجب

---
+++ Development/js/scripts.js
@@ -0,0 +1,48 @@
+let menu= prompt("enter your food")
+
+switch(menu) {
+    case 1:
+      console.log("burger")
+      break;
+    case 2:
+      console.log("meat")
+      break;
+    default:
+        console.log("choose what you want")
+      // code block
+  }
+
+  let operation= prompt("ادخل الرقم الاول")
+  console.log("1")
+  
+  
+  switch(operation){
+      
+      case "+":
+          console.log(number1 + number2);
+          break;
+     case "-":
+        console.log(number2 - number1);
+         break;
+     case "/":
+         console.log(number10 / number5)
+         break;
+    default:
+        console.log("choose what you want")
+let age= ("17")
+    if(age < 5 ){
+        console.log("kg")
+        
+    }
+
+  }
+let age= ("17")
+    if(age < 5 ){
+        console.log("kg")
+    } 
+if( 5< age < 18){
+    console.log("school")
+}
+if(16<age<18){
+    console.log("university")
+}
